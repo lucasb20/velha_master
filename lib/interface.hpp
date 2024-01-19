6cb6df702e3345bce8ab295ecca9817adf12767a
Lucas Rocha <lr359508@gmail.com>
2024-01-18 21:09:21 -0300

    Início do refactory.

--- lib/interface.hpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <cstdlib>
-#include <SDL2/SDL.h>
-#include <vector>
-
-void drawPos(SDL_Window*,SDL_Renderer*,std::vector<char>);
-void drawCircle(SDL_Renderer*,int,int,int);
-char getPlayer(std::vector<char>);
